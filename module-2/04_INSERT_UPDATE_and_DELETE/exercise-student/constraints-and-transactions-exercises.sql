33a5efc01badf023b56a08d2b6b9fb5018145b4c
Jake <jakeschwartz38@gmail.com>
2021-02-11 21:33:30 -0500

    pushing thursday exercise

--- module-2/04_INSERT_UPDATE_and_DELETE/exercise-student/constraints-and-transactions-exercises.sql
+++ module-2/04_INSERT_UPDATE_and_DELETE/exercise-student/constraints-and-transactions-exercises.sql
@@ -2,45 +2,152 @@
 -- The following changes are applied to the "dvdstore" database.**
 
 -- 1. Add actors, Hampton Avenue, and Lisa Byway to the actor table.
+INSERT INTO actor (first_name, last_name)
+VALUES ('HAMPTON','AVENUE'),('LISA','BYWAY');
 
+SELECT *
+FROM actor
+WHERE first_name IN ('HAMPTON','LISA');
 -- 2. Add "Euclidean PI", "The epic story of Euclid as a pizza delivery boy in
 -- ancient Greece", to the film table. The movie was released in 2008 in English.
 -- Since its an epic, the run length is 3hrs and 18mins. There are no special
 -- features, the film speaks for itself, and doesn't need any gimmicks.
+INSERT INTO film(title, description, release_year, language_id, length)
+VALUES ('EUCLIDEAN PI', 'The epic story of Euclid as a pizza delivery boy in ancient Greece', 2008, 1, 198);
+
+SELECT * 
+FROM film
+WHERE title = 'EUCLIDEAN PI';
+
+
 
 -- 3. Hampton Avenue plays Euclid, while Lisa Byway plays his slightly
 -- overprotective mother, in the film, "Euclidean PI". Add them to the film.
 
+INSERT INTO film_actor(actor_id, film_id)
+VALUES((SELECT actor_id FROM actor WHERE actor.first_name = 'HAMPTON' AND actor.last_name ='AVENUE'),(SELECT film_id FROM film WHERE title = 'EUCLIDEAN PI')),
+((SELECT actor_id FROM actor WHERE actor.first_name ='LISA' AND actor.last_name = 'BYWAY'),(SELECT film_id FROM film WHERE title = 'EUCLIDEAN PI'));
+
+SELECT * 
+FROM actor
+JOIN film_actor ON film_actor.actor_id = actor.actor_id
+JOIN film ON film_actor.film_id = film.film_id
+WHERE film.title = 'EUCLIDEAN PI';
+
 -- 4. Add Mathmagical to the category table.
+INSERT INTO category (name)
+VALUES('Mathmagical');
+
+SELECT * FROM category
 
 -- 5. Assign the Mathmagical category to the following films, "Euclidean PI",
 -- "EGG IGBY", "KARATE MOON", "RANDOM GO", and "YOUNG LANGUAGE"
+
+
+
+BEGIN TRANSACTION;
+
+UPDATE film_category
+SET category_id = (SELECT category_id FROM category WHERE category.name = 'Mathmagical')
+FROM film, category
+WHERE film.film_id = film_category.film_id AND film.title IN ('EUCLIDEAN PI', 'EGG IGBY', 'KARATE MOON', 'RANDOM GO', 'YOUNG LANGUAGE');
+
+COMMIT;
+
+SELECT * 
+FROM film
+JOIN film_category ON film.film_id = film_category.film_id
+JOIN category ON film_category.category_id = category.category_id
+WHERE film.title IN ('EUCLIDEAN PI', 'EGG IGBY', 'KARATE MOON', 'RANDOM GO', 'YOUNG LANGUAGE');
+
+
 
 -- 6. Mathmagical films always have a "G" rating, adjust all Mathmagical films
 -- accordingly.
 -- (5 rows affected)
 
+BEGIN TRANSACTION;
+
+UPDATE film
+SET rating = 'G'
+FROM film_category, category
+WHERE film.film_id = film_category.film_id AND film_category.category_id = (SELECT category_id FROM category WHERE name = 'Mathmagical');
+
+COMMIT;
+
+
+SELECT *
+FROM film 
+JOIN film_category ON film_category.film_id = film.film_id
+JOIN category ON category.category_id = film_category.category_id
+WHERE category.name = 'Mathmagical';
+
+
+
 -- 7. Add a copy of "Euclidean PI" to all the stores.
+
+BEGIN TRANSACTION;
+
+INSERT INTO inventory (film_id, store_id)
+VALUES (1001, 1),(1001,2);
+
+
+ROLLBACK;
 
 -- 8. The Feds have stepped in and have impounded all copies of the pirated film,
 -- "Euclidean PI". The film has been seized from all stores, and needs to be
 -- deleted from the film table. Delete "Euclidean PI" from the film table.
 -- (Did it succeed? Why?)
--- <YOUR ANSWER HERE>
+-- No, the film_id is referenced as a foreign key in other tables
+
+BEGIN TRANSACTION;
+
+DELETE FROM film WHERE title = 'EUCLIDEAN PI';
+
+ROLLBACK;
 
 -- 9. Delete Mathmagical from the category table.
 -- (Did it succeed? Why?)
--- <YOUR ANSWER HERE>
+-- No, the category_id is referenced as a foreign key in film_category
+BEGIN TRANSACTION;
+DELETE FROM category WHERE name = 'Mathmagical';
+ROLLBACK;
+
+
 
 -- 10. Delete all links to Mathmagical in the film_category tale.
 -- (Did it succeed? Why?)
--- <YOUR ANSWER HERE>
+-- Yes, because none of the deleted keys are primary, so they exist elsewhere. The films effectively don't have a category anymore, but no discrepencies were created.
+
+BEGIN TRANSACTION;
+DELETE FROM film_category WHERE category_id = (SELECT category_id FROM category WHERE name = 'Mathmagical');
+
+ROLLBACK;
 
 -- 11. Retry deleting Mathmagical from the category table, followed by retrying
 -- to delete "Euclidean PI".
 -- (Did either deletes succeed? Why?)
--- <YOUR ANSWER HERE>
+-- Deleting the category worked, because with all entries in film_category deleted, it's like the category never existed. Deleting the film does not, as it still has keys referenced in film_actor
+
+
+BEGIN TRANSACTION;
+DELETE FROM category WHERE name = 'Mathmagical';
+ROLLBACK;
+
+BEGIN TRANSACTION;
+
+DELETE FROM film WHERE title = 'EUCLIDEAN PI';
+
+ROLLBACK;
+
 
 -- 12. Check database metadata to determine all constraints of the film id, and
 -- describe any remaining adjustments needed before the film "Euclidean PI" can
 -- be removed from the film table.
+
+--All rows containing its film_id would need to be deleted from inventory, film_category (already done), film_actor and language
+
+SELECT pg_constraint.*
+FROM pg_catalog.pg_constraint
+INNER JOIN pg_catalog.pg_class ON pg_class.oid = pg_constraint.conrelid
+INNER JOIN pg_catalog.pg_namespace ON pg_namespace.oid = connamespace;
--- module-2/04_INSERT_UPDATE_and_DELETE/lecture-student/lecture-postgresql.sql
+++ module-2/04_INSERT_UPDATE_and_DELETE/lecture-student/lecture-postgresql.sql
@@ -1,37 +1,98 @@
 -- INSERT
 
 -- 1. Add Klingon as a spoken language in the USA
+INSERT INTO countrylanguage (countrycode, language, isOfficial, percentage)
+VALUES ( 'USA', 'Klingon', false, 5);
+
+SELECT * 
+FROM countrylanguage
+WHERE countrycode = 'USA';
+
 -- 2. Add Klingon as a spoken language in Great Britain (GBR)
 
+INSERT INTO countrylanguage(countrycode, language, isOfficial, percentage)
+VALUES ('GBR', 'Klingon', false, 5);
+
+SELECT * 
+FROM countrylanguage
+WHERE countrycode = 'GBR';
 
 -- UPDATE
 
+--UPDATE table_name
+--SET column to value
+-- WHERE column = value
+
 -- 1. Update the capital of the USA to Houston
+SELECT id
+FROM city
+WHERE name = 'Houston';
+
+UPDATE country
+SET capital=3796
+WHERE code='USA';
+
+SELECT country.code, city.name, headofstate
+FROM country
+INNER JOIN city on country.capital = city.id
+WHERE code = 'USA';
+
 -- 2. Update the capital of the USA to Washington DC and the head of state
+
+UPDATE country
+SET capital= (SELECT id FROM city WHERE name = 'Washington'),
+headofstate = 'Elon Musk'
+Where code = 'USA';
 
 
 -- DELETE
+--DELETE FROM table_name
+--WHERE column = val
 
 -- 1. Delete English as a spoken language in the USA
+DELETE
+--SELECT * 
+FROM countrylanguage 
+WHERE language = 'English' AND countrycode = 'USA';
+
+
 -- 2. Delete all occurrences of the Klingon language 
+DELETE 
+--SELECT *
+FROM countrylanguage
+WHERE language = 'Klingon';
 
 
 -- REFERENTIAL INTEGRITY
 
 -- 1. Try just adding Elvish to the country language table.
+INSERT INTO countrylanguage(language)
+VALUES ('Elvish');
+
 
 -- 2. Try inserting English as a spoken language in the country ZZZ. What happens?
+INSERT INTO countrylanguage(countrycode, language, isOfficial, percentage)
+VALUES ('ZZZ', 'English', false, 5);
 
 -- 3. Try deleting the country USA. What happens?
+DELETE FROM country
+WHERE code = ('USA');
 
 
 -- CONSTRAINTS
 
 -- 1. Try inserting English as a spoken language in the USA
-
+INSERT INTO countrylanguage (countrycode, language, isOfficial, percentage)
+VALUES ('USA', 'English', false, 90)
 -- 2. Try again. What happens?
 
+--fails because it's a duplicate primary key
+
+
 -- 3. Let's relocate the USA to the continent - 'Outer Space'
+UPDATE country
+SET continent = 'Outer Space'
+WHERE code = 'USA';
 
 
 -- How to view all of the constraints
@@ -46,6 +107,139 @@ INNER JOIN pg_catalog.pg_namespace ON pg_namespace.oid = connamespace;
 
 -- 1. Try deleting all of the rows from the country language table and roll it back.
 
+
+
+
 -- 2. Try updating all of the cities to be in the USA and roll it back
 
+START TRANSACTION;
+
+ UPDATE city
+ SET countrycode = 'USA';
+ 
+ SELECT * FROM city;
+ 
+ ROLLBACK;
+ 
+ SELECT * FROM city;
+ 
+
 -- 3. Demonstrate two different SQL connections trying to access the same table where one happens to be inside of a transaction but hasn't committed yet.
+
+START TRANSACTION;
+UPDATE countrylanguage
+SET percentage = 60
+WHERE language = 'English' AND countrycode = 'USA';
+
+
+-----------
+
+-- Write queries to return the following:
+-- Make the following changes in the "world" database.
+
+
+-- 1. Add Superman's hometown, Smallville, Kansas to the city table. The
+-- countrycode is 'USA', and population of 45001.
+INSERT INTO city (name, countrycode, district, population)
+VALUES ('Smallville','USA', 'Kansas', 45001);
+
+SELECT *
+FROM city
+WHERE name = 'Smallville';
+
+
+
+-- 2. Add Kryptonese to the countrylanguage table. Kryptonese is spoken by 0.0001
+-- percentage of the 'USA' population.
+INSERT INTO countrylanguage (countrycode, language, isofficial, percentage)
+VALUES ('USA', 'Kryptonese', false, .0001);
+
+SELECT * 
+FROM countrylanguage
+WHERE countrycode = 'USA';
+
+
+
+-- 3. After heated debate, "Kryptonese" was renamed to "Krypto-babble." Change
+-- the appropriate record accordingly.
+UPDATE countrylanguage
+SET language = 'Krypto-babble'
+WHERE language = 'Kryptonese';
+
+SELECT * 
+FROM countrylanguage
+WHERE countrycode = 'USA';
+
+-- 4. Set the US capital to Smallville, Kansas in the country table.
+
+UPDATE country 
+SET capital =  (SELECT id FROM city WHERE name = 'Smallville')
+WHERE code = 'USA';  
+
+
+SELECT country.name, city.name
+FROM country
+JOIN city ON country.capital = city.id
+WHERE code = 'USA';
+
+-- 5. Delete Smallville, Kansas from the city table. (Did it succeed? Why?)
+DELETE FROM city
+WHERE name = 'Smallville';
+
+--because Smallville is referenced as USA's capital city
+
+-- 6. Return the US capital to Washington.
+UPDATE country
+SET capital = (SELECT id FROM city WHERE name = 'Washington')
+WHERE code = 'USA';
+
+SELECT country.name, city.name
+FROM country
+JOIN city ON country.capital = city.id
+WHERE code = 'USA';
+
+-- 7. Delete Smallville, Kansas from the city table. (Did it succeed? Why?)
+DELETE FROM city
+WHERE name = 'Smallville';
+
+--because smallville is no longer referenced in another table as a foreign key
+
+
+
+-- 8. Reverse the "is the official language" setting for all languages where the
+-- country's year of independence is within the range of 1800 and 1972
+-- (exclusive).
+-- (590 rows affected)
+
+UPDATE countrylanguage
+--JOIN country ON countrylanguage.countrycode = country.code
+SET isofficial = NOT isofficial
+WHERE countrycode IN (SELECT code FROM country WHERE country.indepyear BETWEEN 1800 AND 1972);
+
+
+-- 9. Convert population so it is expressed in 1,000s for all cities. (Round up to
+-- the nearest integer value.)
+-- (4079 rows affected)
+
+
+UPDATE city
+SET population = ROUND((population/1000));
+
+SELECT * FROM city
+
+
+-- 10. Assuming a country's surfacearea is expressed in square miles, convert it to
+-- square meters for all countries where French is spoken by more than 20% of the
+-- population.
+-- (7 rows affected)
+
+UPDATE country
+SET surfacearea = surfacearea * 1609
+WHERE code IN (SELECT countrycode FROM countrylanguage WHERE language = 'French' AND percentage > 20);
+
+
+
+
+
+
+
