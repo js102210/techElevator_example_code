89b1d9905c53814a87fc8387b22ebe1b29f2fda9
Jake <jakeschwartz38@gmail.com>
2021-03-22 23:20:53 -0400

    submitting exercise

--- module-3/11_Introduction_to_Vue_and_Data_Binding/exercise-student/src/components/UserList.vue
+++ module-3/11_Introduction_to_Vue_and_Data_Binding/exercise-student/src/components/UserList.vue
@@ -11,19 +11,25 @@
     </thead>
     <tbody>
       <tr>
-        <td><input type="text" id="firstNameFilter"/></td>
-        <td><input type="text" id="lastNameFilter"/></td>
-        <td><input type="text" id="usernameFilter"/></td>
-        <td><input type="text" id="emailFilter"/></td>
+        <td><input type="text" id="firstNameFilter" v-model="filter.firstName"/></td>
+        <td><input type="text" id="lastNameFilter" v-model="filter.lastName"/></td>
+        <td><input type="text" id="usernameFilter" v-model="filter.username"/></td>
+        <td><input type="text" id="emailFilter" v-model="filter.emailAddress"/></td>
         <td>
-          <select id="statusFilter">
+          <select id="statusFilter" v-model="filter.status">
             <option value="">Show All</option>
             <option value="Active">Active</option>
             <option value="Disabled">Disabled</option>
           </select>
         </td>
       </tr>
-      <!-- user listing goes here -->
+      <tr  class="user" v-for="(user) in filteredList" v-bind:key="user.emailAddress" v-bind:class="{'disabled': user.status == 'Disabled' }">
+        <td>{{user.firstName}} </td>
+        <td>{{user.lastName}}</td>
+        <td>{{user.username}}</td>
+        <td>{{user.emailAddress}}</td>  
+        <td>{{user.status}}</td>
+      </tr>
     </tbody>
   </table>
 </template>
@@ -39,8 +45,32 @@ export default {
         { firstName: 'George', lastName: 'Best', username: 'gbest', emailAddress: 'gbest@gmail.com', status: 'Disabled' },
         { firstName: 'Ben', lastName: 'Carter', username: 'bcarter', emailAddress: 'bcarter@gmail.com', status: 'Active' },
         { firstName: 'Katie', lastName: 'Jackson', username: 'kjackson', emailAddress: 'kjackson@yahoo.com', status: 'Active' },
-        { firstName: 'Mark', lastName: 'Smith', username: 'msmith', emailAddress: 'msmith@foo.com', status: 'Disabled' }
-      ]
+        { firstName: 'Mark', lastName: 'Smith', username: 'msmith', emailAddress: 'msmith@foo.com', status: 'Disabled' },
+      ],
+      filter : {
+        firstName: '',
+        lastName: '',
+        username: '',
+        emailAddress: '',
+        status: ''
+      } 
+    }
+  },
+  computed: {
+    filteredList() {
+      
+      const filterObject = this.filter;
+      let filterFirstName = filterObject.firstName;
+      let filterLastName = filterObject.lastName;
+      let filterUName = filterObject.username;
+      let filterEmail = filterObject.emailAddress;
+      return this.users.filter((user) =>{
+        return user.firstName.toLowerCase().includes(filterFirstName.toLowerCase())&&
+        user.lastName.toLowerCase().includes(filterLastName.toLowerCase())&&
+       user.username.toLowerCase().includes(filterUName.toLowerCase()) &&
+        user.emailAddress.toLowerCase().includes(filterEmail.toLowerCase())&&
+        user.status.includes(filterObject.status);
+      })
     }
   }
 }
